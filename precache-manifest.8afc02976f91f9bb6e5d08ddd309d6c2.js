c43213c4f837b1188bb0732f52175ed9cda06aa4
Jack <jackdmike.settle@gmail.com>
2019-05-20 01:00:00 +0100

    Updates

---
+++ precache-manifest.8afc02976f91f9bb6e5d08ddd309d6c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a359f0c88cb25a4e399f632be6b4a8a",
+    "url": "/stage-bans/index.html"
+  },
+  {
+    "revision": "2ee86756379b79d8f07a",
+    "url": "/stage-bans/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5c6100bd2e682cf0ab6b",
+    "url": "/stage-bans/static/js/2.b48f1146.chunk.js"
+  },
+  {
+    "revision": "2ee86756379b79d8f07a",
+    "url": "/stage-bans/static/js/main.a69a8b47.chunk.js"
+  },
+  {
+    "revision": "730645967ee2f06c4976",
+    "url": "/stage-bans/static/js/runtime~main.a1cd77eb.js"
+  }
+]);
